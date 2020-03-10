e0f80c908435c00db941f8681b2e8e4953ec55f5
Harlintong Urbina Bermudez <harlintonggeonieth@learnet.se>
2020-03-10 13:47:54 +0100

    Extrafiler

---
+++ BoomFight/Assets/Script/Map_Generator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map_Generator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Ground;
+
+    [SerializeField]
+    GameObject Wall;
+
+    [SerializeField]
+    protected int width = 11;
+
+    [SerializeField]
+    protected int length = 11;
+
+    //width och lenght behöver vara ojämna (10 -> 11)
+
+    protected int height = 1;
+
+    protected float offset = 10f;
+
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < length; z++)
+            {
+                GameObject newTile = Instantiate(Ground);
+
+                newTile.transform.position = new Vector3(x, 0, z) * 10;
+
+                newTile.name = "Ground(" + x.ToString() + ", " + z.ToString() + ")";
+
+                newTile.transform.parent = this.transform;
+
+                if (x == 0 || z == 0)
+                {
+                    PlaceWall(offset, x, z);
+                }
+                else if (x == width - 1 || z == length - 1)
+                {
+                    PlaceWall(offset, x, z);
+                }
+                else if (x % 2 == 0 && z % 2 == 0)
+                {
+                    PlaceWall(offset, x, z);
+                }
+            }
+        }
+    }
+
+    protected void PlaceWall(float offset, int x, int z)
+    {
+        GameObject newTile = Instantiate(Wall);
+
+        newTile.transform.position = new Vector3(x, height, z) * 10;
+
+        newTile.name = "Wall(" + x.ToString() + ", " + z.ToString() + ")";
+
+        newTile.transform.parent = this.transform;
+    }
+
+}
---
+++ BoomFight/Assets/Script/Player_Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class Player_Controller : NetworkBehaviour
+{
+    [SerializeField]
+    float MovementSpeed = 3f;
+
+    [SerializeField]
+    float RotationSpeed = 150f;
+
+    [SerializeField]
+    GameObject Bomb;
+
+    [SerializeField]
+    GameObject BombSpawnPoint;
+
+    float TimeBetweenBomb = 1f;
+    float TimeSinceLastBomb = 0f;
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        float yRotation = Input.GetAxisRaw("Horizontal") * RotationSpeed * Time.deltaTime;
+        float zMovement = Input.GetAxisRaw("Vertical") * MovementSpeed * Time.deltaTime;
+
+        Vector3 RotationVector = new Vector3(0, yRotation, 0);
+        Vector3 MovementVector = new Vector3(0, 0, zMovement);
+
+        transform.Rotate(RotationVector);
+        transform.Translate(MovementVector);
+
+        TimeSinceLastBomb += Time.deltaTime;
+    }
+}
